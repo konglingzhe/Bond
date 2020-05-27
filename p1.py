e412edc22bc39892ac58409787a9b235ecf42051
FinTechNJU <FinTechNJU@foxmail.com>
2020-05-28 00:06:51 +0800

    Mac 端更改

--- p1.py
+++ p1.py
@@ -36,14 +36,14 @@ class Bondbook:
             self._bonds[name] = Bond()
         return self._bonds[name]
 # 以下是函数
-class Get_Attributes:
+class GetAttributes:
     @staticmethod
     def get_r( interest_list, price, T):
         # r = (sum(interest_list) - price) / (price * T)
         r = 0.02277500
         return r 
     @staticmethod
-    def get_C1(stock, r):
+    def get_C1(stock, r, K):
         def get_sigma(stock): 
             stock_ln = np.log(stock)
             miu = stock_ln.diff()
@@ -80,7 +80,7 @@ class Get_Attributes:
             total += f(l,n)
         return total
 
-class Global_Functions:
+class GlobalFunctions:
     @staticmethod
     def set_up(i, K):
         # 初始化每一个债券
@@ -90,12 +90,12 @@ class Global_Functions:
         # 所有可转债
         bond1 = book.bond(bond_name)
         # 示例化一个可转债对象
-        book.bond(bond_name).set_r(Get_Attributes.get_r(list1, bond_price , len(stock)))
+        book.bond(bond_name).set_r(GetAttributes.get_r(list1, bond_price , len(stock)))
         # 传入r的值
         bond1.attribute('T').add_value(len(stock))
         bond1.attribute('K').add_value(K)
-        bond1.attribute('C1').add_value(Get_Attributes.get_C1(stock, book.bond(bond_name).r))
-        bond1.attribute('C2').add_value((Get_Attributes.get_C2(list1, book.bond(bond_name).r)))
+        bond1.attribute('C1').add_value(GetAttributes.get_C1(stock, book.bond(bond_name).r, K))
+        bond1.attribute('C2').add_value((GetAttributes.get_C2(list1, book.bond(bond_name).r)))
         # 设置不同属性的值
         value = book.bond(bond_name).attribute('C1').value() + book.bond(bond_name).attribute('C2').value()
         bond1.attribute('Arbitrage').add_value(value - bond_price)
@@ -104,16 +104,22 @@ class Global_Functions:
     def show_info(i):
         bond_name = pd.DataFrame(dfRaw.iloc[:,i]).columns.tolist()[0]
         temp = book.bond(bond_name).attribute('Arbitrage').value()
-        print(temp)
+        # print(temp)
         # 输出可转债的套利空间
-        temp.to_csv('./output/'+bondname+'.csv',mode='w+')
+        temp.to_csv('./output/'+bond_name+'.csv',mode='w+')
         # 可转债的套利空间保存为csv文件
+        print("Done")
+    @staticmethod
+    def read_data():
+        dfRaw = pd.read_csv('./excel1.csv',encoding='gbk').set_index('DateTime')
+        dfRaw2 = pd.read_csv('./excel2.csv',encoding='gbk').set_index('DateTime')
+        return dfRaw, dfRaw2
 
 
 if __name__=='__main__':
     # 主函数 
     if (len(dfRaw)==0) or (len(dfRaw2)==0):
-        dfRaw, dfRaw2 = read_data()
+        dfRaw, dfRaw2 = GlobalFunctions.read_data()
     # 获得数据
 
     r = 0.02277500 # 2020年5月27日的五年期的国债收益率
@@ -128,24 +134,7 @@ if __name__=='__main__':
     list2=[0.4, 0.6, 1.0, 1.6, 2.0, 112.5]
     # 对于每一个可转债
 
-    Global_Functions.set_up(0, K1)
-    Global_Functions.show_info(0)
-
-
-
-
-
-
-
-
-
-
-
-
+    GlobalFunctions.set_up(0, K1)
+    GlobalFunctions.show_info(0)
 
 # %%
-# class Test:
-#     @staticmethod
-#     def f(x):
-#         return x+3
-# a = Test.f(2)

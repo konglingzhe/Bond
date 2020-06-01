ee55fe41aea8724da43cc8ec4e2fba06243b20a9
FinTech_NJU <fintech_nju@foxmail.com>
2020-06-01 12:33:25 +0800

    设计模式与设计原则

--- Test/Test_by_ETF50.py
+++ Test/Test_by_ETF50.py
@@ -4,11 +4,17 @@ Dependence:
     
 '''
 import pandas as pd
-# ============================================================================== #
+import sys
+sys.path.append("..")
+# 调用上一层的文件
+# ------------------------------------------------------------------------------ #
+from WindPy import w
+# ------------------------------------------------------------------------------ #
 from Container import Bondbook
 from Function import GetAttrs
 from Adapter import GlobalFunctions
 # ============================================================================== #
+
 dfRaw, dfRaw2 = pd.DataFrame(), pd.DataFrame()
 # 全局变量
 # ============================================================================== #
@@ -22,10 +28,12 @@ class SetUp:
             time_list: list, 债券开始时间和结束时间列表
         '''
         # 从外部传入数据 TODO 改成函数，不要从文件中读取
-        bond_price = dfRaw.iloc[:,i]
-        bond_name = pd.DataFrame(dfRaw.iloc[:,i]).columns.tolist()[0]
-        stock = dfRaw2.iloc[:,i]
-        # 所有可转债
+        w.start()
+        bond_price = w.wsd("10002339.SH", "us_close", "2019-01-01", "2020-05-29", "",usedf=True)[0]
+        # 50ETF购2020年4月 3.00
+        bond_name = 'ETF50'
+        stock = w.wsd("510050.SH", "nav", "2019-01-01", "2020-05-29", "period=2;returnType=1",usedf=True)[0]
+        # 所有上证ETF50时间序列上的数据
         bond1 = book.bond(bond_name)
         # 实例化一个可转债对象
 
@@ -47,24 +55,6 @@ class SetUp:
         bond1.attr('C1').add_value(GetAttrs.get_C1(stock, bond1.attr('r').value, bond1.attr('K').value, bond1.attr('T').value, bond1.attr('sigma').value))
         # 设置期权价值C1 序列数据
 
-        bond1.attr('C2').add_value(GetAttrs.get_C2(bond1.attr('C0').value, bond1.attr('T').value))
-        # 设置期纯债价值C2 序列数据
-
-        bond1.attr('Arbitrage').add_value(bond1.attr('C1').value + bond1.attr('C2').value - bond_price)
-        # 设置可转债的套利属性
-
-        bond1.attr('Bond_Value').add_value(GetAttrs.get_Bond_Value(bond1.attr('C1').value, bond1.attr('C2').value))
-        # 设置可转债的价值
-
-        bond1.attr('Value_Series').add_value(GetAttrs.get_Value_Series(bond1.attr('K').value, stock))
-        # 设置可转债的转股价值
-
-        bond1.attr('Premium_Rate').add_value(GetAttrs.get_Premium_Rate(bond_price, bond1.attr('Value_Series').value))
-        # 设置可转债的转股溢价率
-
-        bond1.attr('Stock_Price').add_value(stock)# 设置正股股价
-        bond1.attr('Bond_Price').add_value(bond_price)# 设置可转债价格
-
 # ============================================================================== #
 
 if __name__=='__main__':
@@ -77,8 +67,7 @@ if __name__=='__main__':
     # 设置可转债的基本指标
     # 对于所有可转债
 
-    list1=[0.4, 0.6, 1.0, 1.6, 2.0, 112.5] # 债券1的利息表
-    # K1 = 37.97 # 债券1的行权价
+
     K1_dict = {
         '2019-02-15':37.97,
         '2019-05-31':22.28,
@@ -87,6 +76,19 @@ if __name__=='__main__':
     # 债券1的行权价
     time_list = ['2019-02-15', '2025-02-15']
     # 债券1的开始时间和结束时间
-    list2=[0.4, 0.6, 1.0, 1.6, 2.0, 112.5]
-    K2 = 20.22 # 债券2的行权价
-    # 对于每一个可转债
+
+    SetUp.set_up(0, K1_dict, time_list)
+    GlobalFunctions.show_info(book, 0)
+    print("Function: __main__ ... Done!")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
